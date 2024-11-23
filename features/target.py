2da6314764db55d1e37ceb0e716c3bd2c8b5998d
Mridula Shankar <mridulas_macair@Mridulas-MacBook-Air.local>
2024-11-22 19:40:27 -0800

    Modified few lines after getting help from mentors

--- features/target.py
+++ features/target.py
@@ -16,10 +16,14 @@ driver.get('https://www.target.com/')
 
 # Search
 # driver.find_element(By.ID, 'search').send_keys('tea')
-driver.find_element(By.XPATH, "//span[@class='sc-58ad44c0-3 kwbrXj h-margin-r-x3']").click()
-sleep(5)
+# driver.find_element(By.XPATH, "//span[@class='sc-58ad44c0-3 kwbrXj h-margin-r-x3']").click()
+driver.find_element(By .XPATH,"//a[@data-test='@web/AccountLink']").click()
+sleep(2)
 driver.find_element(By.XPATH,"//button[@data-test='accountNav-signIn']").click()
-sleep(5)
+# driver.find_element(By .XPATH, "//a[@data-test='accountNav-signIn']").click()
+sleep(2)
+
+driver.find_element(By.ID, 'login')
 
 # Verification:
 expected_result = 'Sign into your Target account'
@@ -27,6 +31,7 @@ actual_result = driver.find_element(By.XPATH, "//span").text
 
 # assert expected_result in actual_result, f'Expected text {expected_result} not in actual {actual_result}'
 print('Test case passed')
+driver.quit()
 
 # Verify an element present:
 # driver.find_element(By.XPATH, "//*[text()='Popular filters']")
